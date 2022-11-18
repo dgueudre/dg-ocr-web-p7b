de2c3e4eea25d2ba58f08c6d78975113a98e3932
dgueudre <dgueudre@gmail.com>
2022-11-18 20:57:26 +0100

    style

--- apps/web/src/app/AccommodationCard.tsx
+++ apps/web/src/app/AccommodationCard.tsx
@@ -1,10 +1,44 @@
 import { Accommodation } from '@kasa/data';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: grid;
+  align-items: flex-end;
+  height: 100%;
+  overflow: hidden;
+  background-color: #FF6060;
+  border-radius: 10px;
+  
+  &>p {
+    grid-area: 1/1;
+    z-index: 2;
+    color: white;
+  }
+  
+  &>img {
+    grid-area: 1/1;
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    /* opacity: 0.95; */
+  }
+
+  &::after {
+    grid-area: 1/1;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    content: "";
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%);
+  }
+`;
 
 export default function AccommodationCard({ accommodation }:{ accommodation: Accommodation }) {
   return (
-    <div>
+    <Card>
       <p>{accommodation.title}</p>
       <img src={accommodation.cover} alt="" srcSet="" />
-    </div>
+    </Card>
   );
 }
--- apps/web/src/app/AccommodationGallery.tsx
+++ apps/web/src/app/AccommodationGallery.tsx
@@ -1,16 +1,27 @@
 import { Accommodation } from '@kasa/data';
+import styled from 'styled-components';
 import AccommodationCard from './AccommodationCard';
+
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  &>li {
+    margin: 1.6%;
+    width: 30%;
+  }
+`;
 
 export default function AccommodationGallery(
   { accommodations }: { accommodations: Accommodation[] },
 ) {
   return (
-    <ul>
+    <List>
       {accommodations.map(((accommodation) => (
         <li>
           <AccommodationCard accommodation={accommodation} />
         </li>
       )))}
-    </ul>
+    </List>
   );
 }
