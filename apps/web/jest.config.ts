2bac6529a80aeb2b04adc2e974441a6d5452ef4a
dgueudre <dgueudre@gmail.com>
2022-11-18 18:45:24 +0100

    build: rename project

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'dgueudre',
+  displayName: 'web',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/dgueudre',
+  coverageDirectory: '../../coverage/apps/web',
 };
