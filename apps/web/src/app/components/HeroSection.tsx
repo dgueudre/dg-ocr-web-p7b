ae5bd9c8c0ac40ad56d7d7c92751d19729f87118
dgueudre <dgueudre@gmail.com>
2022-11-19 11:40:17 +0100

    hero-section

---
+++ apps/web/src/app/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+import './HeroSection.scss';
+
+export default function HeroSection() {
+  return (
+    <section className="hero-section">
+      <h1>Chez vous, partout et ailleurs</h1>
+      <img src="/assets/hero-img.png" alt="" />
+    </section>
+  );
+}
--- apps/web/src/app/pages/HomePage.tsx
+++ apps/web/src/app/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { data } from '@kasa/data';
 import AccommodationGallery from '../components/AccommodationGallery';
+import HeroSection from '../components/HeroSection';
 
 export default function HomePage() {
   return (
     <div>
       <h1>Home</h1>
+      <HeroSection />
       <AccommodationGallery accommodations={data} />
     </div>
   );
